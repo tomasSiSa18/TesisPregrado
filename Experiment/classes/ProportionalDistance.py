9ea872b85c3425e04f5dec1f0513719c5465937f
Tomas Sierra <t.sierras@uniandes.edu.co>
2026-09-02 17:21:38 -0500

    Fo sure

--- Experiment/classes/ProportionalDistance.py
+++ Experiment/classes/ProportionalDistance.py
@@ -8,6 +8,9 @@ class ProportionalDistance:
     
     def dPD(self, rho: np.ndarray, sigma: np.ndarray):
 
+        r = rho.copy()
+        s = sigma.copy()
+
         max: float = 0.0
         lam = math.inf
         
@@ -17,17 +20,25 @@ class ProportionalDistance:
             
             max_i = None
             for i in range(rho.shape[0]):
-                div = rho[i][i]/sigma[i][i]
+                div = r[i][i]/s[i][i]
                 if max_i is None:
                     max_i = div
                 else:
                     if div > max_i :
                         max_i = div 
+            max = max_i
             
-            eta = rho.copy() - max_i*sigma.copy()
+            eta = r.copy() - max_i*s.copy()
             _, P = np.linalg.eigh(eta)
-            print()
             P_t = np.conjugate(P).T
+            
+            r = P_t * r * P
+            s = P_t * s * P
+           
+        print("Done") 
+        return math.log(max)
+            
+            
             
             
     
